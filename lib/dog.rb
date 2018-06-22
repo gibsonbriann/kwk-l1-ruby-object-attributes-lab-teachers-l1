9dd06e2750540717d9abc637221073272e695c6a
BriAnn Nicole Gibson <gibsonbriann@gmail.com>
2018-06-22 15:03:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# dog.rb
+class Dog
+ def name
+   @name=name 
+ end
+
+  
+  @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
-# person.rb
+class Person
+  def name 
+    @name=name
+  end
+    
+    
+    @name =name 
+    
+    def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+  end
+end
